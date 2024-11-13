3c1328001dac2532d809a8cbacf1388e8243629e
assiszang <assiszang@gmail.com>
2024-11-13 09:03:37 -0300

    feat: Add docker settings

--- Program.cs
+++ Program.cs
@@ -2,34 +2,35 @@ using Microsoft.EntityFrameworkCore;
 using ReminderList.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://*:80");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Configure the in-memory database
 builder.Services.AddDbContext<TodoContext>(options =>
-    options.UseInMemoryDatabase("TodoList"));
+	options.UseInMemoryDatabase("TodoList"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}")
-	.WithStaticAssets();
-
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
--- commands.md
+++ commands.md
@@ -10,4 +10,14 @@ dotnet new mvc -o ReminderList -au none
 dotnet add package Microsoft.EntityFrameworkCore.InMemory
 dotnet add package Microsoft.EntityFrameworkCore
 ```
+## Create docker image 
 
+```bash
+docker build -t todoapp .  
+```
+
+## Run docker container
+
+```bash
+docker run -d -p 8080:80 --name todoapp-container todoapp
+```
